c2538d06e80165a1eddfd7893b7e793d702bd2fd
ld0532182 <email@email.com>
2021-05-06 16:19:23 +0300

    Changed to LinkedList. Iterator added.

--- src/com/javalessons/collections/lists/ToDoList.java
+++ src/com/javalessons/collections/lists/ToDoList.java
@@ -1,38 +1,62 @@
 package com.javalessons.collections.lists;
 
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ToDoList {
 
-    private ArrayList<String> toDoList = new ArrayList<>();
+    private LinkedList<String> toDoList = new LinkedList<>();
 
     public void addToList(String task) {
-        toDoList.add(task);
+//      toDoList.add(task);
+        addInAlphabeticalOrder(task);
+    }
+
+    private boolean addInAlphabeticalOrder(String task) {
+        ListIterator<String> listIterator = toDoList.listIterator();
+        while (listIterator.hasNext()) {
+            int compared = listIterator.next().compareTo(task);
+            if (compared == 0) {
+                System.out.println("Task has already existed");
+                return true;
+            } else if (compared > 0) {
+                listIterator.previous();
+                listIterator.add(task);
+                return true;
+            }
+        }
+        listIterator.add(task);
+        return true;
     }
 
     public void addToListAtPosition(int pos, String task) {
         toDoList.add(pos, task);
     }
 
-    public void printToDoList(){
-        for (int i = 0; i < toDoList.size(); i++){
+    public void printToDoList() {
+/*        for (int i = 0; i < toDoList.size(); i++){
             System.out.println("#"+i+" - "+toDoList.get(i));
+        }*/
+        Iterator<String> iterator = toDoList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Element " + iterator.next());
         }
     }
 
-    public void changeTask(int pos, String task){
+    public void changeTask(int pos, String task) {
         toDoList.set(pos, task);
     }
 
-    public void removeTask(int index){
+    public void removeTask(int index) {
         toDoList.remove(index);
     }
 
-    public void removeTask(String task){
+    public void removeTask(String task) {
         toDoList.remove(task);
     }
 
-    public int getPriority(String task){
+    public int getPriority(String task) {
         return toDoList.indexOf(task);
     }
 
